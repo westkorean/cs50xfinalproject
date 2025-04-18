152eb6864fac01a528e5554b5e3997086cf5efb5
Ryan Guan <ryanguan0000@gmail.com>
2025-04-18 02:13:49 +0000

    test

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to Expo Router 👋</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, justifyContent: 'center', alignItems: 'center'
+  },
+  text: {
+    fontSize: 20
+  }
+});
